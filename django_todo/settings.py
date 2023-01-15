d1e3c714cb49a253dedf74edde9b39e1d7a5467d
araldwin <araldwin@gmail.com>
2023-01-15 18:25:14 +0000

    set up environment viriables and automatic deployment.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ara143135-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://qlxoduzo:97zzzMcOWaumqyu_WSnEiEvXUZhM6Fh1@mel.db.elephantsql.com/qlxoduzo')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
